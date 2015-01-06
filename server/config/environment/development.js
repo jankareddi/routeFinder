d057117ec8322e2f65e0fbb30c8c80181305c398
jankareddi <jankareddi@gmail.com>
2015-01-06 18:54:12 +0530

    fix in URL construction

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -5,7 +5,7 @@
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri:    'mongodb://' + process.env.RFDB_PORT_27017_TCP_ADDR + ':' process.env.RFDB_PORT_27017_TCP_PORT + '/routefinder-dev' ||
+    uri:    'mongodb://' + process.env.RFDB_PORT_27017_TCP_ADDR + ':' + process.env.RFDB_PORT_27017_TCP_PORT + '/routefinder-dev' ||
             'mongodb://localhost/routefinder-dev'
   },
 
