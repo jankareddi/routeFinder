93fd9e1fb49b18c04e52d9848959fcd87bef91c3
jankareddi <jankareddi@gmail.com>
2015-01-07 19:44:29 +0530

    updated local config to not use docker conf

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -1,11 +1,22 @@
 'use strict';
 
+var _ = require('lodash');
+
 // Development specific configuration
 // ==================================
+
+function getDockerUri() {
+  var dockerUri;
+  if (!(_.isUndefined(process.env.RFDB_PORT_27017_TCP_ADDR) || _.isUndefined(RFDB_PORT_27017_TCP_PORT)))
+    dockerUri = 'mongodb://' + process.env.RFDB_PORT_27017_TCP_ADDR + ':' + process.env.RFDB_PORT_27017_TCP_PORT + '/routefinder-dev';
+
+  return dockerUri;
+}
+
 module.exports = {
   // MongoDB connection options
   mongo: {
-    uri:    'mongodb://' + process.env.RFDB_PORT_27017_TCP_ADDR + ':' + process.env.RFDB_PORT_27017_TCP_PORT + '/routefinder-dev' ||
+    uri:    getDockerUri() ||
             'mongodb://localhost/routefinder-dev'
   },
 
